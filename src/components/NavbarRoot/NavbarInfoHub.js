c7251a0b159b3f473fca38eeec2522cf8fe5645e
Lisa Wagner <lisawagner.online@gmail.com>
2022-06-22 19:10:51 -0700

    Feat: Add InfoHub Scaffolding to NavRoot

--- src/components/NavbarRoot/NavbarInfoHub.js
+++ src/components/NavbarRoot/NavbarInfoHub.js
@@ -1,8 +1,15 @@
 import React from 'react'
+// styles
+import styles from './NavbarInfoHub.module.css'
+import { RiInformationLine } from "react-icons/ri";
+
+// add tooltip
 
 const NavbarInfoHub = () => {
   return (
-    <div>NInfoHub</div>
+    <div>
+      <RiInformationLine className={styles.infoHubIcon}/>
+    </div>
   )
 }
 
