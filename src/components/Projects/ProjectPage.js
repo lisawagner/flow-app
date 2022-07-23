22ad214dfd634a0baadd0d4f49c16bf3c9872cfc
Lisa Wagner <lisawagner.online@gmail.com>
2022-07-23 13:48:11 -0700

    style: add / refactor styles for projects page

--- src/components/Projects/ProjectPage.js
+++ src/components/Projects/ProjectPage.js
@@ -1,20 +1,113 @@
 import styles from './ProjectPage.module.css'
+import { Link } from 'react-router-dom'
+
+// icons
+import { RiFolder3Fill, RiLock2Line } from "react-icons/ri";
+
+// TODO: Extract tiles into their own component
 
 const ProjectPage = () => {
   return (
     <div>
-      <div>Project Cards/Tiles</div>
       <div className={styles.pPageWrap}>
         <div className={styles.pPageContainer}>
-          <div className={styles.pTileWrap}>
-            <div className={styles.pTileCard}>
-              <div className={styles.pTileCardIcon}>
-                iconImage
-              </div>
-              <div className={styles.pTileCardName}>Project Name</div>
 
+          <div className={styles.pTileWrap}>
+            <div className={styles.pTileCardWrap}>
+              <div className={styles.pTileCardContainer}>
+                <Link to='/home' className={styles.pCardLinkWrap}>
+                  
+                  <div className={styles.pCardLinkContainer}>
+                    <div className={styles.pCardLinkWrap}>
+                      <div className={styles.pCardIconWrap}>
+                        <RiFolder3Fill className={styles.pIcon} />
+                      </div>
+                    </div>
+                    <div className={styles.pTileCardName}>Project Name</div>
+                    <div className={styles.pCardSubTitle}>
+                      <RiLock2Line />
+                    </div>
+                  </div>
+
+                </Link>
+              </div>
             </div>
           </div>
+
+          <div className={styles.pTileWrap}>
+            <div className={styles.pTileCardWrap}>
+              <div className={styles.pTileCardContainer}>
+                <Link to='/home' className={styles.pCardLinkWrap}>
+                  <div className={styles.pCardLinkContainer}>
+                    <div className={styles.pCardLinkWrap}>
+                      <div className={styles.pCardIconWrap}>
+                        <RiFolder3Fill className={styles.pIcon} />
+                      </div>
+                    </div>
+                    <div className={styles.pTileCardName}>Project Name</div>
+                    <div className={styles.pCardSubTitle}></div>
+                  </div>
+                </Link>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.pTileWrap}>
+            <div className={styles.pTileCardWrap}>
+              <div className={styles.pTileCardContainer}>
+                <Link to='/home' className={styles.pCardLinkWrap}>
+                  <div className={styles.pCardLinkContainer}>
+                    <div className={styles.pCardLinkWrap}>
+                      <div className={styles.pCardIconWrap}>
+                        <RiFolder3Fill className={styles.pIcon} />
+                      </div>
+                    </div>
+                    <div className={styles.pTileCardName}>Project Name</div>
+                    <div className={styles.pCardSubTitle}></div>
+                  </div>
+                </Link>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.pTileWrap}>
+            <div className={styles.pTileCardWrap}>
+              <div className={styles.pTileCardContainer}>
+                <Link to='/home' className={styles.pCardLinkWrap}>
+                  <div className={styles.pCardLinkContainer}>
+                    <div className={styles.pCardLinkWrap}>
+                      <div className={styles.pCardIconWrap}>
+                        <RiFolder3Fill className={styles.pIcon} />
+                      </div>
+                    </div>
+                    <div className={styles.pTileCardName}>Project Name</div>
+                    <div className={styles.pCardSubTitle}></div>
+                  </div>
+                </Link>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.pTileWrap}>
+            <div className={styles.pTileCardWrap}>
+              <div className={styles.pTileCardContainer}>
+                <Link to='/home' className={styles.pCardLinkWrap}>
+                  <div className={styles.pCardLinkContainer}>
+                    <div className={styles.pCardLinkWrap}>
+                      <div className={styles.pCardIconWrap}>
+                        <RiFolder3Fill className={styles.pIcon} />
+                      </div>
+                    </div>
+                    <div className={styles.pTileCardName}>Project Name</div>
+                    <div className={styles.pCardSubTitle}>
+                      <RiLock2Line />
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
