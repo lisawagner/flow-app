1be3ef8b3b9341a194d57c524e8b06ca97efabd2
Lisa Wagner <lisawagner.online@gmail.com>
2022-07-24 14:21:51 -0700

    chore: clean up code

--- src/components/NavbarRoot/NavbarRoot.js
+++ src/components/NavbarRoot/NavbarRoot.js
@@ -10,21 +10,16 @@ import styles from './NavbarRoot.module.css'
 const NavbarRoot = () => {
   return (
     <div className={styles.nRootWrap}>
-      {/* <h1>NRoot</h1> */}
       <div className={styles.nHeaderTop}>
-        {/* <span className={styles.nItem}></span> */}
         <div className={styles.nHeader}><NavbarHeader /></div>
         <span className={styles.nItem}></span>
         <div className={styles.nMenu}><NavbarMenu /></div>
-        {/* <span className={styles.nItem}></span> */}
         {/* <div className={styles.nPriorities}><NavbarPriorities /></div> */}
-        
       </div>
       
       <div className={styles.nHeaderBottom}>
         <span className={styles.nItem}></span>
-        <div className={styles.nInfoHub}><NavbarInfoHub /></div> 
-        
+        <div className={styles.nInfoHub}><NavbarInfoHub /></div>   
       </div>
 
        
