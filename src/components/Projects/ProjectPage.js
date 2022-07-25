26bf98b9c4a9b387d2885d3a62ed1db4ace696a2
Lisa Wagner <lisawagner.online@gmail.com>
2022-07-25 11:23:23 -0700

    feat: Add array mapping for projects

--- src/components/Projects/ProjectPage.js
+++ src/components/Projects/ProjectPage.js
@@ -1,117 +1,57 @@
-import styles from './ProjectPage.module.css'
 import { Link } from 'react-router-dom'
+import NoProjects from './NoProjects';
+
+// styles
+import styles from './ProjectPage.module.css'
 
 // icons
 import { RiFolder3Fill, RiLock2Line } from "react-icons/ri";
 
-// TODO: Extract tiles into their own component
+// TODO:  Refactor code into re-usable components
+//        'ButtonView' and 'ListView' for projects, plus NoProjectsView
 
-const ProjectPage = () => {
-  return (
-    <div>
-      <div className={styles.pPageWrap}>
-        <div className={styles.pPageContainer}>
+const ProjectPage = ({ projects }) => {
+  const allProjects = projects
 
-          <div className={styles.pTileWrap}>
-            <div className={styles.pTileCardWrap}>
-              <div className={styles.pTileCardContainer}>
-                <Link to='/home' className={styles.pCardLinkWrap}>
-                  
-                  <div className={styles.pCardLinkContainer}>
-                    <div className={styles.pCardLinkWrap}>
-                      <div className={styles.pCardIconWrap}>
-                        <RiFolder3Fill className={styles.pIcon} />
+  if (projects.length === 0) {
+    return <NoProjects />
+  } else {
+    return (
+      
+      <div>
+        <div className={styles.pPageWrap}>
+          <div className={styles.pPageContainer}>
+  
+            {allProjects.map(project => (
+            <div key={project.id} className={styles.pTileWrap}>
+              <div className={styles.pTileCardWrap}>
+                <div className={styles.pTileCardContainer}>
+                  <Link to={`/streams/${project.id}`}  className={styles.pCardLinkWrap}>
+                    
+                    <div className={styles.pCardLinkContainer}>
+                      <div className={styles.pCardLinkWrap}>
+                        <div className={styles.pCardIconWrap}>
+                          <RiFolder3Fill className={styles.pIcon} />
+                        </div>
+                      </div>
+                      <div className={styles.pTileCardName}>{project.name}</div>
+                      <div className={styles.pCardSubTitle}>
+                        {/* <RiLock2Line /> */}
                       </div>
                     </div>
-                    <div className={styles.pTileCardName}>Project Name</div>
-                    <div className={styles.pCardSubTitle}>
-                      <RiLock2Line />
-                    </div>
-                  </div>
-
-                </Link>
+  
+                  </Link>
+                </div>
               </div>
             </div>
+            ))}
+  
           </div>
-
-          <div className={styles.pTileWrap}>
-            <div className={styles.pTileCardWrap}>
-              <div className={styles.pTileCardContainer}>
-                <Link to='/home' className={styles.pCardLinkWrap}>
-                  <div className={styles.pCardLinkContainer}>
-                    <div className={styles.pCardLinkWrap}>
-                      <div className={styles.pCardIconWrap}>
-                        <RiFolder3Fill className={styles.pIcon} />
-                      </div>
-                    </div>
-                    <div className={styles.pTileCardName}>Project Name</div>
-                    <div className={styles.pCardSubTitle}></div>
-                  </div>
-                </Link>
-              </div>
-            </div>
-          </div>
-
-          <div className={styles.pTileWrap}>
-            <div className={styles.pTileCardWrap}>
-              <div className={styles.pTileCardContainer}>
-                <Link to='/home' className={styles.pCardLinkWrap}>
-                  <div className={styles.pCardLinkContainer}>
-                    <div className={styles.pCardLinkWrap}>
-                      <div className={styles.pCardIconWrap}>
-                        <RiFolder3Fill className={styles.pIcon} />
-                      </div>
-                    </div>
-                    <div className={styles.pTileCardName}>Project Name</div>
-                    <div className={styles.pCardSubTitle}></div>
-                  </div>
-                </Link>
-              </div>
-            </div>
-          </div>
-
-          <div className={styles.pTileWrap}>
-            <div className={styles.pTileCardWrap}>
-              <div className={styles.pTileCardContainer}>
-                <Link to='/home' className={styles.pCardLinkWrap}>
-                  <div className={styles.pCardLinkContainer}>
-                    <div className={styles.pCardLinkWrap}>
-                      <div className={styles.pCardIconWrap}>
-                        <RiFolder3Fill className={styles.pIcon} />
-                      </div>
-                    </div>
-                    <div className={styles.pTileCardName}>Project Name</div>
-                    <div className={styles.pCardSubTitle}></div>
-                  </div>
-                </Link>
-              </div>
-            </div>
-          </div>
-
-          <div className={styles.pTileWrap}>
-            <div className={styles.pTileCardWrap}>
-              <div className={styles.pTileCardContainer}>
-                <Link to='/home' className={styles.pCardLinkWrap}>
-                  <div className={styles.pCardLinkContainer}>
-                    <div className={styles.pCardLinkWrap}>
-                      <div className={styles.pCardIconWrap}>
-                        <RiFolder3Fill className={styles.pIcon} />
-                      </div>
-                    </div>
-                    <div className={styles.pTileCardName}>Project Name</div>
-                    <div className={styles.pCardSubTitle}>
-                      <RiLock2Line />
-                    </div>
-                  </div>
-                </Link>
-              </div>
-            </div>
-          </div>
-
         </div>
       </div>
-    </div>
-  )
+    )
+  }
+
 }
 
 export default ProjectPage
