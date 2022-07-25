db261853025ebd72bb5ea019a8180ff54f537f4c
Lisa Wagner <lisawagner.online@gmail.com>
2022-07-25 11:22:08 -0700

    feat: Add NoProjects component to Projects Page

---
+++ src/components/Projects/NoProjects.js
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom'
+// styles
+import styles from './NoProjects.module.css'
+// icons
+import { RiDraftLine } from "react-icons/ri";
+
+const NoProjects = () => {
+  return (
+    <div className={styles.noProjectWrap}>
+      <RiDraftLine className={styles.noProjectIcon}/>
+      <h1>Ready to create your first project?</h1>
+      <p className={styles.hideOnSmall}>You do not have any active projects in right now.</p>
+      <p className={styles.hideOnSmall}>Go ahead and create your first project.</p>
+      <Link to='/create'>
+        <button type="button" tabIndex="0" className={styles.noProjectBtn}>
+          <span>Create Project</span>
+        </button>
+      </Link>
+      
+    </div>
+  )
+}
+
+export default NoProjects
