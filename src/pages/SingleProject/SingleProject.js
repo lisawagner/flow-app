105f184ff381835e83c0e6bb537ffebe0b04596f
Lisa Wagner <lisawagner.online@gmail.com>
2022-07-25 17:19:28 -0700

    feat: fetch single project data

--- src/pages/SingleProject/SingleProject.js
+++ src/pages/SingleProject/SingleProject.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { useDocument } from '../../hooks/useDocument'
+import { useTitle } from '../../context/TitleContext'
+
+// styles
 import styles from './SingleProject.module.css'
 
 const SingleProject = () => {
+  const { id } = useParams()
+  const { error, userDoc } = useDocument('projects', id)
+  const [projectTitle, setProjectTitle] = useState('')
+  const { setTitle } = useTitle()
+
+  // useEffect(() => {
+    
+  //   setTitle(projectTitle)
+  // }, [setTitle, projectTitle])
+
+  // Time for React-Helmet
+
+  
+  if (error) {
+    return <div className="error">{error}</div>
+  }
+  if (!userDoc) {
+    return <div className="loading">Loading...</div>
+  }
+
   return (
-    <div>SingleProject</div>
+    <div>
+      <h1>{userDoc.name}</h1>
+
+    </div>
   )
 }
 
