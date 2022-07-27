93828b8a7574c0d60c7c691d678ca3edd3f51a75
Lisa Wagner <lisawagner.online@gmail.com>
2022-07-27 08:15:06 -0700

    styles: update UI styles

--- src/components/TabNavBar/TabNavBar.js
+++ src/components/TabNavBar/TabNavBar.js
@@ -10,17 +10,17 @@ const TabNavBar = () => {
     <div className={styles.tabNavBarWrap}>
       <div className={styles.tabNavBarContainer}>
         <div className={styles.tabNavItem}>
-          <NavLink to="overview">
+          <NavLink to="overview" className={styles.tabNavItemLink}>
             <span>Overview</span>
           </NavLink>
         </div>
-        <div className={styles.tabNavItem}>
-          <NavLink to="list">
+        <div className={styles.tabNavItem} >
+          <NavLink to="list" className={styles.tabNavItemLink}>
             <span>List</span>
           </NavLink>
         </div>
         <div className={styles.tabNavItem}>
-          <NavLink to="board">
+          <NavLink to="board" className={styles.tabNavItemLink}>
             <span>Board</span>
           </NavLink>
         </div>
