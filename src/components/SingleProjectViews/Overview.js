db49756a12801d4091fc0c166b13c4a66983252a
Lisa Wagner <lisawagner.online@gmail.com>
2022-07-27 12:16:44 -0700

    styles: refactor overview styles

--- src/components/SingleProjectViews/Overview.js
+++ src/components/SingleProjectViews/Overview.js
@@ -1,33 +1,40 @@
 import { useOutletContext } from 'react-router-dom';
 import Avatar from '../Avatar/Avatar'
+import Chat from './Chat';
 // styles
 import styles from './Overview.module.css'
 
 
 const Overview = () => {
   const { userDoc } = useOutletContext()
-  console.log("OutletContext: ", userDoc);
+  // console.log("OutletContext: ", userDoc);
   return (
     <div className={styles.overWrap}>
       <div className={styles.overContainer}>
+        
         <div className={styles.summaryWrap}>
-          <h2>Summary</h2>
-          <p>{userDoc.details}</p>
-        </div>
-        <div className={styles.teamWrap}>
-          <h2>Project Team</h2>
-          <div className={styles.assignedUsers}>
-            {userDoc.assignedUsersList.map(user => (
-              <div key={user.id}>
-                <Avatar src={user.photoURL} />
+          <div className={styles.summaryContent}>
+            <div>
+              <h2>Summary</h2>
+              <p>{userDoc.details}</p>
+            </div>
+            <div>
+              <h2>Project Team</h2>
+              <div className={styles.assignedUsers}>
+                {userDoc.assignedUsersList.map(user => (
+                  <div key={user.id}>
+                    <Avatar src={user.photoURL} />
+                  </div>
+                ))}
               </div>
-            ))}
+            </div>
           </div>
         </div>
+
         <div className={styles.msgsWrap}>
-          <h2>Messages</h2>
-          <p>{userDoc.category}</p>
+          <Chat document={userDoc}/>
         </div>
+
       </div>
     </div>
   )
