f8143f913b1fc8418e2b5f5aa78f86356bf5462a
Lisa Wagner <lisawagner.online@gmail.com>
2022-07-30 12:07:37 -0700

    feat: add board view scaffolding

---
+++ src/components/Board/TaskItem.js
@@ -0,0 +1,51 @@
+import Avatar from '../Avatar/Avatar';
+import { useAuthContext } from '../../hooks/useAuthContext'
+// styles
+import styles from './TaskItem.module.css'
+// icons
+import { RiCheckboxCircleFill, RiCheckboxCircleLine } from "react-icons/ri";
+// ^ switch between icon types onclick (isDone, setIsDone)
+
+// TODO: add onhover effect to show task menu: edit/delete/markcomplete button
+
+const TaskItem = () => {
+  const { user } = useAuthContext()
+
+  return (
+    <div className={styles.boardCardWrap}>
+
+      <div className={styles.boardCardContainer}>
+        <div className={styles.boardCardContent}>
+          <div className={styles.boardCardHeader}>
+            <div className={styles.completionIndicator}>
+              <RiCheckboxCircleLine />
+            </div>
+            {/* <h3>TaskItemName</h3> */}
+            <span>TaskItemName Task with long name, so long it's a bit much? Hah, for now, you can make the longest task item evah!</span>
+          </div>
+          {/* <span>createdAt (needed?)</span> */}
+          <div className={styles.priorities}>
+            <div className={styles.cellWrapper}>
+              <div className={styles.pill}>priorityLevel</div>
+            </div>
+          </div>
+          <div className={styles.boardCardFooter}>
+            <div className={styles.boardCardAssignees}>
+              <div className={styles.assigneeAvatar}>
+              <Avatar src={user.photoURL}/> 
+              </div>
+            </div>
+            <div className={styles.dueDateWrap}>
+              <div className={styles.dueDateContainer}>
+                <span>dueDate</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default TaskItem
--- src/components/SingleProjectViews/BoardView.js
+++ src/components/SingleProjectViews/BoardView.js
@@ -1,8 +1,72 @@
-import React from 'react'
+import { useOutletContext } from 'react-router-dom';
+// components
+import TaskItem from '../Board/TaskItem';
+// styles
+import styles from './BoardView.module.css'
 
 const BoardView = () => {
+  const { userDoc } = useOutletContext()
+  console.log("BoardView OutletContext: ", userDoc);
   return (
-    <div>BoardView</div>
+    <div>
+      <h1>BoardView</h1>
+      <p>Each project will have a bunch of tasks. Tasks will be sortable by Col/category and can be moved between categories/columns.</p>
+      <div>Columns - task categories/dynamic aka BoardColumnHeader</div>
+
+      <div className={styles.boardContainer}>
+        <div className={styles.boardColumnWrap}>
+
+          <div className={styles.boardColumnContainer}>
+            <div className={styles.boardColumnHeader}>
+              <h2>ToDo</h2>  
+            </div>
+            <div className={styles.boardColumnContent}>
+              <TaskItem /> 
+              <TaskItem />
+              <TaskItem />
+            </div>
+          </div>
+
+          <div className={styles.boardColumnContainer}>
+            <div className={styles.boardColumnHeader}>
+              <h2>In Progress</h2>  
+            </div>
+            <div className={styles.boardColumnContent}>
+              <TaskItem /> 
+              <TaskItem />
+              <TaskItem />
+            </div>
+          </div>
+
+          <div className={styles.boardColumnContainer}>
+            <div className={styles.boardColumnHeader}>
+              <h2>Done</h2>  
+            </div>
+            <div className={styles.boardColumnContent}>
+              <TaskItem /> 
+              <TaskItem />
+              <TaskItem />
+            </div>
+          </div>
+
+          <div className={styles.boardColumnContainer}>
+            <div className={styles.boardColumnHeader}>
+              <h2>Done</h2>  
+            </div>
+            <div className={styles.boardColumnContent}>
+              <TaskItem /> 
+              <TaskItem />
+              <TaskItem />
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+      {/* <div>Tasks</div>
+      <TaskItem /> */}
+      
+    </div>
   )
 }
 
